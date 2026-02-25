725a85222a96b952744576f5226308edb9e29725
mtmsujon <mtmsujon@gmail.com>
2026-02-25 15:26:36 +0600

    add class wise product free shipping

--- includes/class-ajax-handler.php
+++ includes/class-ajax-handler.php
@@ -36,6 +36,9 @@ class Advanced_Checkout_Ajax
 
         // Provide a filter to auto-select payment gateway if desired
         add_filter('woocommerce_available_payment_gateways', [$this, 'filter_gateways']);
+
+        // Product wise free delivery filter
+        add_filter('woocommerce_package_rates', [$this, 'dynamic_free_shipping_by_class'], 100, 2);
     }
 
     /**
@@ -89,4 +92,51 @@ class Advanced_Checkout_Ajax
 
         return $available_gateways;
     }
+
+    /**
+     * Product wise free delivery function
+     * 
+     * If any product in the cart has the shipping class 'free-shipping',
+     * all other shipping rates are removed and a custom Free Shipping rate is added.
+     * 
+     * @param array $rates
+     * @param array $package
+     * @return array
+     */
+    public function dynamic_free_shipping_by_class($rates, $package)
+    {
+        $free_shipping_class = 'free-shipping'; // Ensure this matches your Shipping Class slug
+        $found = false;
+
+        // Check items in the shipping package
+        foreach ($package['contents'] as $item) {
+            $product = $item['data'];
+
+            if ($product && $product->get_shipping_class() === $free_shipping_class) {
+                $found = true;
+                break;
+            }
+        }
+
+        if ($found) {
+            $free_shipping_label = esc_html__('ফ্রি ডেলিভারি', 'advanced-checkout');
+
+            // Remove all existing shipping methods
+            $rates = array();
+
+            // Create the new Free Shipping rate
+            $new_rate = new WC_Shipping_Rate();
+            $new_rate->set_id('dynamic_free_shipping');
+            $new_rate->set_method_id('free_shipping');
+            $new_rate->set_instance_id(0);
+            $new_rate->set_label($free_shipping_label);
+            $new_rate->set_cost(0);
+            $new_rate->set_taxes(array());
+
+            $rates['dynamic_free_shipping'] = $new_rate;
+        }
+
+        return $rates;
+    }
+
 }
